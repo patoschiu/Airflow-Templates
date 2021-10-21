9c2f6800fe5a37dc047ea1f6a91c0ef7f8fd773a
patoschiu <91089144+patoschiu@users.noreply.github.com>
2021-10-21 18:09:54 -0300

    Update hello_airflow.py

--- hello_airflow.py
+++ hello_airflow.py
@@ -6,10 +6,10 @@ from airflow.operators.python_operator import PythonOperator
 def print_hello():
     return 'Hello world from first Airflow DAG!'
 
-dag = DAG('hello_world', description='Hello World DAG',
+dag = DAG('hello_airflow', description='Hello World DAG',
           schedule_interval='0 12 * * *',
           start_date=datetime(2017, 3, 20), catchup=False)
 
-hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello, dag=dag)
+hello_operator = PythonOperator(task_id='hello_airflow', python_callable=print_hello, dag=dag)
 
 hello_operator
