21863f03a9f6e338d6ef7cbd97ecb869cb83b862
Patricio Schiumerini <patricioschiumerini@Patricios-MBP.fibertel.com.ar>
2021-10-22 08:18:45 -0300

    Hello postgres dag

--- hello_postgres.py
+++ hello_postgres.py
@@ -16,7 +16,7 @@ def create_sql_file(csv_path, sql_path, table_name):
 
 with DAG(
     dag_id="postgres_operator_dag",
-    start_date=datetime.datetime(2020, 2, 2),
+    start_date=datetime(2020, 2, 2),
     schedule_interval="@once",
     catchup=False,
 ) as dag:
