1ae46a6e095f1d5bbb32c171d1005f8716f0c3b5
Patricio Schiumerini <patricioschiumerini@Patricios-MacBook-Pro.local>
2021-10-26 13:10:38 -0300

    Test postgres hook

--- hello_postgres.py
+++ hello_postgres.py
@@ -9,7 +9,7 @@ from airflow.operators.python_operator import PythonOperator
 from airflow.hooks.postgres_hook import PostgresHook
 
 
-def create_sql_statement(ti):
+def insert_csv_into_postgres():
     sql_statement = ""
     csv_input_filepath = os.path.join(os.path.dirname(os.path.realpath(__file__)),'csv','deniro.csv')
     table_name = "movies"
@@ -30,6 +30,17 @@ def create_sql_statement(ti):
     #ti.xcom_push(key='sql_load_table', value=sql_statement)
 
 
+def print_postgres_table():
+    request = "SELECT * FROM movies:"
+    ps_hook = PostgresHook()
+    connection = ps_hook.get_conn()
+    cursor = connection.cursor()
+    cursor.execute(request)
+    response = cursor.fetchall()
+    for row in response:
+        print(row)
+
+
 with DAG(
     dag_id="hello_postgres",
     start_date=datetime(2020, 2, 2),
@@ -40,6 +51,7 @@ with DAG(
         task_id="create_movies_table",
         postgres_conn_id="postgres_default",
         sql="""
+            DROP TABLE movies;
             CREATE TABLE IF NOT EXISTS movies (
             Year VARCHAR NOT NULL,
             Score VARCHAR NOT NULL,
@@ -47,9 +59,14 @@ with DAG(
         """
     )
 
-    create_sql_file_task = PythonOperator(
-        task_id='create_sql_statement',
-        python_callable=create_sql_statement,
+    insert_csv_into_postgres_task = PythonOperator(
+        task_id='insert_csv_into_postgres',
+        python_callable=insert_csv_into_postgres,
+    )
+
+    print_postgres_table_task = PythonOperator(
+        task_id='print_postgres_table',
+        python_callable=print_postgres_table,
     )
 
     #populate_movies_table_task = PostgresOperator(
@@ -58,5 +75,5 @@ with DAG(
     #    sql="sql/deniro/sql"
     #)
 
-    create_movies_table_task >> create_sql_file_task 
+    create_movies_table_task >> insert_csv_into_postgres_task  >> print_postgres_table_task
     #>> populate_movies_table_task
